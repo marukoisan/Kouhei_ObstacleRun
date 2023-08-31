c5570ffbc8c40d89c8f3e0941ca91c45fb07d8ca
maruyamakouhei <107020852+marukoisan@users.noreply.github.com>
2023-08-31 16:12:47 +0900

    途中です

---
+++ Source/Kouhei_ObstacleRun/Public/CPPRotatingComponent.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CPPRotatingComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class KOUHEI_OBSTACLERUN_API UCPPRotatingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+
+	UPROPERTY(EditAnywhere, DisplayName = "Interval", Category = "Rotate Settings")
+		float Interval = 0.01f;
+
+	UPROPERTY(EditAnywhere, DisplayName = "Rot(X)", Category = "Rotate Settings")
+		float RotX = 0.0f;
+
+	UPROPERTY(EditAnywhere, DisplayName = "Rot(Y)", Category = "Rotate Settings")
+		float RotY = 0.0f;
+
+	UPROPERTY(EditAnywhere, DisplayName = "Rot(Z)", Category = "Rotate Settings")
+		float RotZ = 0.0f;
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	FRotator OwnerRotation;
+
+	void RotateLoop();
+};
